6174050e84477bf2f3a158f158844ad5c27e53ed
osi <yaoiluo@gmail.com>
2018-07-07 11:34:18 +0800

    FEINIT

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.app')
+
+@section('htmlheader_title')
+管理后台
+@endsection
+
+@section('main-content')
+<div class="row">
+  <div class="col-md-12">
+   <div class="panel panel-default">
+    <div class="panel-heading">Dashboard</div>
+    <div class="panel-body">
+     欢迎使用 管理后台
+   </div>
+ </div>
+</div>
+</div>
+@endsection
---
+++ resources/views/admin/partials/htmlheader.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--[if IE 9]>         <html class="ie9 no-focus"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-focus"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>西南石大甲子校庆 - @yield('htmlheader_title', '')</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0">
+  <meta name="renderer" content="webkit">
+  <meta name="force-rendering" content="webkit">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <!-- Bootstrap and OneUI CSS framework -->
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" id="css-main" href="/assets/css/oneui.css">
+  <link rel="stylesheet" href="/assets/js/plugins/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css">
+  <!-- <link rel="stylesheet" id="css-theme" href="/assets/css/themes/flat.min.css"> -->
+  <script src="/assets/js/core/jquery.min.js"></script>
+</head>
